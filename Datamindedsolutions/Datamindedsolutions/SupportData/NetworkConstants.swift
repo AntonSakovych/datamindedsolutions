ac636b574a1b082faf248c23392c9b4866702017
Anton Sakovych <Anton.Sakovych@onseo.biz>
2019-07-01 23:49:10 +0300

    Setup Project

---
+++ Datamindedsolutions/Datamindedsolutions/SupportData/NetworkConstants.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkConstants.swift
+//  RedRooster
+//
+//  Created by Alex Zhurba on 2/7/19.
+//  Copyright © 2019 Anton Sakovych. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkConstans {
+    static var baseURL = "https://www.reddit.com/r/all/top/.json?t=all&limit=10"
+}
+
