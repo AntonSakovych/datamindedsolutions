ec4b589f04a932f1895ca5e789d6aa7abf99028c
Anton Sakovych <Anton.Sakovych@onseo.biz>
2019-07-01 22:39:45 +0300

    Initial commit

---
+++ Datamindedsolutions/Datamindedsolutions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Datamindedsolutions
+//
+//  Created by Anton Sakovych on 7/1/19.
+//  Copyright © 2019 Anton Sakovych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
