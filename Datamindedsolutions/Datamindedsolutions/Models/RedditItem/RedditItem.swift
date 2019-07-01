a8a7a71872bfb8b73c88daf716470d37a4e55371
Anton Sakovych <Anton.Sakovych@onseo.biz>
2019-07-02 00:09:57 +0300

    Setup TableView

---
+++ Datamindedsolutions/Datamindedsolutions/Models/RedditItem/RedditItem.swift
@@ -0,0 +1,9 @@
+//
+//  RedditItem.swift
+//  Datamindedsolutions
+//
+//  Created by Anton Sakovych on 7/2/19.
+//  Copyright © 2019 Anton Sakovych. All rights reserved.
+//
+
+import Foundation
---
+++ Datamindedsolutions/Datamindedsolutions/Views/Cells/TableViewCell/TableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewCell.swift
+//  Datamindedsolutions
+//
+//  Created by Anton Sakovych on 7/2/19.
+//  Copyright © 2019 Anton Sakovych. All rights reserved.
+//
+
+import Foundation
